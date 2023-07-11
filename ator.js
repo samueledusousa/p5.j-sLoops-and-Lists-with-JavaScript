1d7436ea18b62e11955612fb8561ab9f66534ea1
Samuel E. Sousa <132164905+samueledusousa@users.noreply.github.com>
2023-07-10 23:45:05 -0300

    Add files via upload

---
+++ ator.js
@@ -0,0 +1,65 @@
+//Código do Ator
+
+let xAtor = 88;
+let yAtor = 366;
+let colisao = false;
+let meusPontos = 0;
+
+function mostraAtor(){
+  image(imagemDoAtor, xAtor, yAtor, 30, 30);
+}
+
+function movimentaAtor(){
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    if(podeSeMover()){
+    yAtor += 3;
+    }
+  }
+}
+
+function verificaColisao(){
+  //collideRectCircle(x1, y1, width1, heigth1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i++){
+   colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, AlturaCarro, xAtor, yAtor, 15) 
+    if (colisao){
+      voltaAtorParaPosicaoInicial();
+      somDaColisao.play();
+      if (pontosMaiorQueZero()){
+      meusPontos -=1;
+      }
+    }  
+  }
+}
+
+function voltaAtorParaPosicaoInicial(){
+  yAtor = 366;
+}
+
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25)
+  fill(color(127, 255, 0))
+  text(meusPontos, width / 5, 27);
+}
+
+function marcaPonto(){
+  if (yAtor < 9){
+    meusPontos += 1;
+    somDoPonto.play();
+    voltaAtorParaPosicaoInicial();
+  }
+}
+
+function pontosMaiorQueZero(){
+  return meusPontos > 0
+}
+
+function podeSeMover(){
+  return yAtor < 366;
+}
+
+
+
